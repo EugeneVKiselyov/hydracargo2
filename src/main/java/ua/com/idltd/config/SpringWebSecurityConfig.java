e414e9a718059e1cfc748a5cb18275ec531df4f7
e.kiselyov <eugene.kiselyov@gmail.com>
2019-11-28 06:47:50 +0200

    Добавил workplace

--- src/main/java/ua/com/idltd/config/SpringWebSecurityConfig.java
+++ src/main/java/ua/com/idltd/config/SpringWebSecurityConfig.java
@@ -4,37 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SpringWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Qualifier("dataSource")
-    @Autowired
-    DataSource dataSource;
+    private final DataSource dataSource;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    public SpringWebSecurityConfig() {
+    @Autowired
+    public SpringWebSecurityConfig(@Qualifier("dataSource") DataSource dataSource) {
         super();
+        this.dataSource = dataSource;
     }
 
     @Override
@@ -45,8 +39,8 @@ public class SpringWebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                     .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/")
-                    .failureUrl("/login-error.html")
+                    .defaultSuccessUrl("/login-success")
+                    .failureUrl("/login-error")
                     .permitAll()
                 .and()
                     .logout()
